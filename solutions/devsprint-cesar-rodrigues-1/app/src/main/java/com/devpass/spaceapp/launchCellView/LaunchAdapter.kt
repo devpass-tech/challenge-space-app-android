8ab58cd1c463b2a0420e28abbb74ffad9a79ec2d
Lucas Simão <oliveira.lucas20@gmail.com>
2022-04-14 20:58:42 -0300

    Moves companion object : DiffUtil to LaunchModel and implements binding in LaunchViewHolder

--- solutions/devsprint-cesar-rodrigues-1/app/src/main/java/com/devpass/spaceapp/launchCellView/LaunchAdapter.kt
+++ solutions/devsprint-cesar-rodrigues-1/app/src/main/java/com/devpass/spaceapp/launchCellView/LaunchAdapter.kt
@@ -2,15 +2,11 @@ package com.devpass.spaceapp.launchCellView
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.devpass.spaceapp.R
 import com.devpass.spaceapp.databinding.ListItemBinding
 
-class LaunchAdapter : ListAdapter<LaunchModel, LaunchAdapter.LaunchViewHolder>(LaunchAdapter) {
+class LaunchAdapter : ListAdapter<LaunchModel, LaunchViewHolder>(LaunchModel) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LaunchViewHolder {
         return LaunchViewHolder.from(parent)
@@ -19,43 +15,32 @@ class LaunchAdapter : ListAdapter<LaunchModel, LaunchAdapter.LaunchViewHolder>(L
     override fun onBindViewHolder(holder: LaunchViewHolder, position: Int) {
         holder.bind(getItem(position))
     }
+}
 
-    private companion object : DiffUtil.ItemCallback<LaunchModel>() {
+class LaunchViewHolder(binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    private val imageLaunch = binding.ivLogo
+    private val nameLaunch = binding.tvName
+    private val numberLaunch = binding.tvNumber
+    private val dateLaunch = binding.tvDate
+    private val statusLaunch = binding.tvStatus
 
-        override fun areItemsTheSame(oldItem: LaunchModel, newItem: LaunchModel): Boolean {
-            return oldItem.number == newItem.number
-        }
-
-        override fun areContentsTheSame(oldItem: LaunchModel, newItem: LaunchModel): Boolean {
-            return oldItem == newItem
+    fun bind(model: LaunchModel) {
+        with(itemView) {
+            imageLaunch.setImageResource(model.image)
+            nameLaunch.text = model.name
+            numberLaunch.text = model.number
+            dateLaunch.text = model.date
+            statusLaunch.text = model.status
         }
     }
 
-    class LaunchViewHolder(binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root) {
-        private val imageLaunch = itemView.findViewById<ImageView>(R.id.iv_logo)
-        private val nameLaunch = itemView.findViewById<TextView>(R.id.tv_name)
-        private val numberLaunch = itemView.findViewById<TextView>(R.id.tv_number)
-        private val dateLaunch = itemView.findViewById<TextView>(R.id.tv_date)
-        private val statusLaunch = itemView.findViewById<TextView>(R.id.tv_status)
-
-        fun bind(model: LaunchModel) {
-            with(itemView) {
-                imageLaunch.setImageResource(model.image)
-                nameLaunch.text = model.name
-                numberLaunch.text = model.number
-                dateLaunch.text = model.date
-                statusLaunch.text = model.status
-            }
-        }
-
-        companion object {
-            fun from(parent: ViewGroup): LaunchViewHolder {
-                return LaunchViewHolder(
-                    ListItemBinding.inflate(
-                        LayoutInflater.from(parent.context), parent, false
-                    )
+    companion object {
+        fun from(parent: ViewGroup): LaunchViewHolder {
+            return LaunchViewHolder(
+                ListItemBinding.inflate(
+                    LayoutInflater.from(parent.context), parent, false
                 )
-            }
+            )
         }
     }
 }
