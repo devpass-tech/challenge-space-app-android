2c6ab8ad8eaf60c9b10d47a9adf6ca63dfd0891b
Lucas Simão <oliveira.lucas20@gmail.com>
2022-04-27 18:11:57 -0300

    Creates an abstract class for the states

--- solutions/devsprint-cesar-rodrigues-1/app/src/main/java/com/devpass/spaceapp/presentation/States.kt
+++ solutions/devsprint-cesar-rodrigues-1/app/src/main/java/com/devpass/spaceapp/presentation/States.kt
@@ -1,9 +1,11 @@
 package com.devpass.spaceapp.presentation
 
-import com.devpass.spaceapp.data.model.LaunchModel
+import com.devpass.spaceapp.data.model.launch.LaunchModel
 
-sealed class States {
-    object Loading : States()
-    data class Success(val launches: LaunchModel) : States()
-    data class Failure(val failure: String?) : States()
+abstract class States {
+    sealed class LaunchStates {
+        object Loading : LaunchStates()
+        data class Success(val launches: LaunchModel) : LaunchStates()
+        data class Failure(val failure: String?) : LaunchStates()
+    }
 }
