cc9da8ab8ed3b719264e517d41019aaa9d50462b
Ronan Fernandes <ronanlm10@hotmail.com>
2022-04-28 11:03:34 -0300

    Config TabLayout e ViewPager

---
+++ solutions/devsprint-cesar-rodrigues-1/app/src/main/java/com/devpass/spaceapp/presentation/FragmentDetails.kt
@@ -0,0 +1,31 @@
+package com.devpass.spaceapp.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.devpass.spaceapp.databinding.FragmentDetailsBinding
+
+class FragmentDetails: Fragment() {
+
+    private var binding: FragmentDetailsBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding?.root
+    }
+}
