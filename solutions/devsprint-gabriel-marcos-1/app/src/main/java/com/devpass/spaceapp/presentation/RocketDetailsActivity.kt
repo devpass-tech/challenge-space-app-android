4f299d1975a7d960e6743f924e3fb62b75e97963
Flávio Júnior <flaviojunior.work@gmail.com>
2022-05-20 14:50:40 -0300

    Drop method onStart() in RocketDetailsActivity.kt

--- solutions/devsprint-gabriel-marcos-1/app/src/main/java/com/devpass/spaceapp/presentation/RocketDetailsActivity.kt
+++ solutions/devsprint-gabriel-marcos-1/app/src/main/java/com/devpass/spaceapp/presentation/RocketDetailsActivity.kt
@@ -20,17 +20,14 @@ class RocketDetailsActivity : AppCompatActivity() {
         val drawable = ContextCompat.getDrawable(this, R.drawable.ic_arrow_back)
         supportActionBar?.setHomeAsUpIndicator(drawable)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-    }
-
-    override fun onStart() {
-        super.onStart()
         setup()
     }
 
     private fun setup() {
         binding.apply {
             name.text = "Falcon Heavy"
-            details.text = "With the ability to lift into orbit over 54 metric tons (119,000 lb)--a mass equivalent to a 737 jetliner loaded with passengers, crew, luggage and fuel--Falcon Heavy can lift more than twice the payload of the next closest operational vehicle, the Delta IV Heavy, at one-third the cost."
+            details.text =
+                "With the ability to lift into orbit over 54 metric tons (119,000 lb)--a mass equivalent to a 737 jetliner loaded with passengers, crew, luggage and fuel--Falcon Heavy can lift more than twice the payload of the next closest operational vehicle, the Delta IV Heavy, at one-third the cost."
         }
     }
 
