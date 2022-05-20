b1177296a33eba692074e64ae621fc0d4a44aef4
Paula Ribeiro <rpaulaeduarda@gmail.com>
2022-05-20 19:10:40 -0300

    Implementa a LaunchTabActivity

---
+++ solutions/devsprint-gabriel-marcos-1/app/src/main/java/com/devpass/spaceapp/presentation/LaunchpadFragment.kt
@@ -0,0 +1,8 @@
+package com.devpass.spaceapp.presentation
+
+import androidx.fragment.app.Fragment
+import com.devpass.spaceapp.R
+
+class LaunchpadFragment : Fragment(R.layout.fragment_launchpad) {
+
+}
