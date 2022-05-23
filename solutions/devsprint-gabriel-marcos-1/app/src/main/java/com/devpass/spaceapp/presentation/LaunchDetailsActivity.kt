b2407d0faa0a4efaff7a6a7aa0b7aafd78124caf
rentakelly <rentakelly@hotmail.com>
2022-05-23 18:08:08 -0300

    Add tela de detalhes

--- solutions/devsprint-gabriel-marcos-1/app/src/main/java/com/devpass/spaceapp/presentation/LaunchDetailsActivity.kt
+++ solutions/devsprint-gabriel-marcos-1/app/src/main/java/com/devpass/spaceapp/presentation/LaunchDetailsActivity.kt
@@ -1,4 +1,12 @@
 package com.devpass.spaceapp.presentation
 
-class LaunchDetailsActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.devpass.spaceapp.R
+
+class LaunchDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.launch_datail_activity)
+    }
 }
