3051daaac443ea7d09fa2320559cb4e690b7b891
josecarlosj <junior.edf@gmail.com>
2022-05-24 17:58:41 -0300

    Adicionados valores de SUCCESS e FAILURE do lançamento como constantes no LaunchAdapterItem

--- solutions/devsprint-gabriel-marcos-1/app/src/main/java/com/devpass/spaceapp/presentation/LaunchAdapterItem.kt
+++ solutions/devsprint-gabriel-marcos-1/app/src/main/java/com/devpass/spaceapp/presentation/LaunchAdapterItem.kt
@@ -7,7 +7,11 @@ import com.bumptech.glide.Glide
 import com.devpass.spaceapp.data.model.NextLaunchesModel
 import com.devpass.spaceapp.databinding.LaunchAdapterItemBinding
 
+private const val SUCCESS_LAUNCH = "Success"
+private const val FAILURE_LAUNCH = "Failure"
+
 class LaunchAdapterItem(private val nextLaunches: List<NextLaunchesModel>) : RecyclerView.Adapter<LaunchAdapterItem.LauncherItemHolder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LauncherItemHolder {
         val itemBinding = LaunchAdapterItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return LauncherItemHolder(itemBinding)
@@ -23,8 +27,8 @@ class LaunchAdapterItem(private val nextLaunches: List<NextLaunchesModel>) : Rec
     class LauncherItemHolder(private val itemBinding: LaunchAdapterItemBinding) : RecyclerView.ViewHolder(itemBinding.root) {
         fun bind(nextLaunch: NextLaunchesModel){
             itemBinding.launchName.text = nextLaunch.name
-            //itemBinding.launchDate.text = //todo: check format to convert
-            itemBinding.launchStatus.text = if (nextLaunch.status) "Success" else "Failure"
+            itemBinding.launchDate.text = nextLaunch.date
+            itemBinding.launchStatus.text = if (nextLaunch.status) SUCCESS_LAUNCH  else FAILURE_LAUNCH
             itemBinding.launchNumber.text = "#${nextLaunch.flightNumber}"
 
             Glide
