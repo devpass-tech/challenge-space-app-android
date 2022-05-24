f5aa24f92ac2648d1d6c38fd6feef9bb4290795e
Paula Ribeiro <rpaulaeduarda@gmail.com>
2022-05-24 19:05:59 -0300

    Implementa card de details

--- solutions/devsprint-gabriel-marcos-1/app/src/main/java/com/devpass/spaceapp/presentation/DetailsFragment.kt
+++ solutions/devsprint-gabriel-marcos-1/app/src/main/java/com/devpass/spaceapp/presentation/DetailsFragment.kt
@@ -1,8 +1,35 @@
 package com.devpass.spaceapp.presentation
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.devpass.spaceapp.R
+import com.devpass.spaceapp.databinding.FragmentDetailsBinding
 
-class DetailsFragment : Fragment(R.layout.fragment_details) {
+class DetailsFragment : Fragment() {
 
+    private lateinit var binding: FragmentDetailsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        binding = FragmentDetailsBinding.inflate(inflater, container, false)
+
+        configureClickable()
+
+        return binding.root
+    }
+
+    private fun configureClickable() {
+        binding.textViewMore.setOnClickListener {
+            val intent = Intent(context, LaunchDetailsActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
