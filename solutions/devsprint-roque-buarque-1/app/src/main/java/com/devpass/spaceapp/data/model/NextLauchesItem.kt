ea9ce2613abfad0b6e4f11042fc0442d36743d73
BruMedeiross <bru.medeiross@hotmail.com>
2022-06-03 19:46:26 -0300

    ajustes código

--- solutions/devsprint-roque-buarque-1/app/src/main/java/com/devpass/spaceapp/data/model/NextLauchesItem.kt
+++ solutions/devsprint-roque-buarque-1/app/src/main/java/com/devpass/spaceapp/data/model/NextLauchesItem.kt
@@ -8,10 +8,15 @@ import kotlinx.android.parcel.Parcelize
 @Parcelize
 data class NextLauchesItem(
 
-    @SerializedName("name") val name: String?,
-    @SerializedName("date_local") val dateLocal: String?,
-    @SerializedName("flight_number") val flightNumber: Int?,
-    @SerializedName("links")  val links: Links?,
-    @SerializedName("success") val success: Boolean?
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("date_local")
+    val dateLocal: String,
+    @SerializedName("flight_number")
+    val flightNumber: Int,
+    @SerializedName("links")
+    val links: Links,
+    @SerializedName("success")
+    val success: Boolean
 
 ):Parcelable
