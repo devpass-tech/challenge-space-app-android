22c109787bcd7ff84dd42878ada2969c90a82773
BruMedeiross <bru.medeiross@hotmail.com>
2022-06-06 10:32:48 -0300

    ajustes pr2

--- solutions/devsprint-roque-buarque-1/app/src/main/java/com/devpass/spaceapp/data/model/NextLauchesItem.kt
+++ solutions/devsprint-roque-buarque-1/app/src/main/java/com/devpass/spaceapp/data/model/NextLauchesItem.kt
@@ -1,9 +1,9 @@
 package com.devpass.spaceapp.data.model
 
 
-import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
-import kotlinx.android.parcel.Parcelize
+import kotlinx.serialization.Serializable
+
 
 @Serializable
 data class NextLauchesItem(
