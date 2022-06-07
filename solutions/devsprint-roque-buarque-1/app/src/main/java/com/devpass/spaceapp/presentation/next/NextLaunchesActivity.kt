d278156dec4287de125cb645cbee81cc2af074e8
denislima <denis.lima@capgemini.com>
2022-06-06 21:48:12 -0300

    Merged

--- solutions/devsprint-roque-buarque-1/app/src/main/java/com/devpass/spaceapp/presentation/next/NextLaunchesActivity.kt
+++ solutions/devsprint-roque-buarque-1/app/src/main/java/com/devpass/spaceapp/presentation/next/NextLaunchesActivity.kt
@@ -2,13 +2,15 @@ package com.devpass.spaceapp.presentation.next
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.devpass.spaceapp.R
 import com.devpass.spaceapp.data.repository.next.NextLaunchRepository
+import com.devpass.spaceapp.databinding.ActivityMainBinding
+import com.devpass.spaceapp.presentation.viewBinding
 
 class NextLaunchesActivity : AppCompatActivity() {
+    private val nextLaunchesActivityBinding by viewBinding(ActivityMainBinding::inflate)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(nextLaunchesActivityBinding.root)
 
         val repository = NextLaunchRepository.newInstance()
     }
--- solutions/devsprint-roque-buarque-1/app/src/main/java/com/devpass/spaceapp/presentation/rocket/RocketDetailsActivity.kt
+++ solutions/devsprint-roque-buarque-1/app/src/main/java/com/devpass/spaceapp/presentation/rocket/RocketDetailsActivity.kt
@@ -1,4 +1,15 @@
 package com.devpass.spaceapp.presentation.rocket
 
-class RocketDetailsActivity {
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.devpass.spaceapp.R
+import com.devpass.spaceapp.databinding.ActivityRocketDetailsBinding
+
+class RocketDetailsActivity : AppCompatActivity() {
+    private val rocketDetailsActivityBinding by viewBinding(ActivityRocketDetailsBinding::inflate)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(rocketDetailsActivityBinding.root)
+    }
 }
