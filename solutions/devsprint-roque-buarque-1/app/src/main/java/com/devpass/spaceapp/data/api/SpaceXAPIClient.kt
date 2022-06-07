56d28ef0565d5ca6b1dcc0554692083601612506
colussicode <you@example.com>
2022-06-07 12:44:21 -0300

    creating fetchLaunchpadDetails inside our interface

--- solutions/devsprint-roque-buarque-1/app/src/main/java/com/devpass/spaceapp/data/api/SpaceXAPIClient.kt
+++ solutions/devsprint-roque-buarque-1/app/src/main/java/com/devpass/spaceapp/data/api/SpaceXAPIClient.kt
@@ -1,9 +1,14 @@
 package com.devpass.spaceapp.data.api
 
 import com.devpass.spaceapp.data.NextLaunchModel
+import com.devpass.spaceapp.data.model.Launchpad
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface SpaceXAPIService {
     @GET("next")
-     suspend fun fetchNextLaunch() : NextLaunchModel
+    suspend fun fetchNextLaunch() : NextLaunchModel
+
+    @GET("launchpads")
+    suspend fun fetchLaunchPadDetails(@Query("id") id: String) : Launchpad
 }
