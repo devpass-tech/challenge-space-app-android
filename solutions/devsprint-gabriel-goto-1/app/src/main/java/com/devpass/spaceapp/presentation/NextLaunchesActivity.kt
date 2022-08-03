547e439f56d19aaef608a19ea38f32a89626411c
Rodrigo Borges <rborges.soares@gmail.com>
2022-08-03 14:37:04 -0300

    Base project for Dev Sprint Gabriel Goto #1

---
+++ solutions/devsprint-gabriel-goto-1/app/src/main/java/com/devpass/spaceapp/presentation/NextLaunchesActivity.kt
@@ -0,0 +1,12 @@
+package com.devpass.spaceapp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.devpass.spaceapp.R
+
+class NextLaunchesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
