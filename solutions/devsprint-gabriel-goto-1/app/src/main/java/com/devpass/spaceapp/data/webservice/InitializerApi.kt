65396507a81e847b552b27c987909acf3626875a
Camila Fonseca <camfsilva@hotmail.com>
2022-08-09 19:32:11 -0300

    add libs to gradle

--- solutions/devsprint-gabriel-goto-1/app/src/main/java/com/devpass/spaceapp/data/webservice/InitializerApi.kt
+++ solutions/devsprint-gabriel-goto-1/app/src/main/java/com/devpass/spaceapp/data/webservice/InitializerApi.kt
@@ -1,4 +1,26 @@
 package com.devpass.spaceapp.data.webservice
 
+import com.devpass.spaceapp.data.api.SpaceXAPIClient
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object InitializerApi {
+
+    private const val BASE_URL = "https://api.spacexdata.com/"
+
+    fun create(): SpaceXAPIClient {
+
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.HEADERS)
+        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        return retrofit.create(SpaceXAPIClient::class.java)
+    }
 }
