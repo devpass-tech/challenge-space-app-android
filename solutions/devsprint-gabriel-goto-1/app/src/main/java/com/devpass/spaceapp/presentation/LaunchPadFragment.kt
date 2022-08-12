e8b9bab77e539805fbfafe66b201de3eeebad64b
Camila Fonseca <camfsilva@hotmail.com>
2022-08-12 19:54:03 -0300

    Add the fragment of launch pad

---
+++ solutions/devsprint-gabriel-goto-1/app/src/main/java/com/devpass/spaceapp/presentation/LaunchPadFragment.kt
@@ -0,0 +1,34 @@
+package com.devpass.spaceapp.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.devpass.spaceapp.databinding.FragmentLaunchPadBinding
+
+class LaunchPadFragment : Fragment() {
+
+    private var bind: FragmentLaunchPadBinding? = null
+    private val binding get() = bind!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        bind = FragmentLaunchPadBinding.inflate(inflater, container, false)
+        setUpView()
+
+        return binding.root
+    }
+
+    private fun setUpView() {
+        binding.textViewRocketName.text = ""
+        binding.textViewBase.text = ""
+        binding.textViewCountry.text = ""
+        binding.buttonViewMore.setOnClickListener {
+            //TODO - fazer a navegacao para a prox view
+        }
+    }
+}
