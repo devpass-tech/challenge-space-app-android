3ac4d8c94cbecd8d2421d5d255514229bef9b988
vitoriaseabra <vitoriaseabra_@hotmail.com>
2022-08-12 17:49:14 -0300

    LaunchCellItem Implementation

---
+++ solutions/devsprint-gabriel-goto-1/app/src/main/java/com/devpass/spaceapp/presentation/LaunchCellItemViewHolder.kt
@@ -0,0 +1,6 @@
+package com.devpass.spaceapp.presentation
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class LaunchCellItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
