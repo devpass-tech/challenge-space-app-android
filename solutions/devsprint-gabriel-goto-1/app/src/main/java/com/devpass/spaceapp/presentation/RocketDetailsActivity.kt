8aaf828c4bdda2b4f31c3cf0bdf1afd688eec71b
Lucas Emboava <emboavalucas@hotmail.com>
2022-08-13 01:55:29 -0300

    Implementação do RocketDetailsActivity.kt e do respectivo layout.

--- solutions/devsprint-gabriel-goto-1/app/src/main/java/com/devpass/spaceapp/presentation/RocketDetailsActivity.kt
+++ solutions/devsprint-gabriel-goto-1/app/src/main/java/com/devpass/spaceapp/presentation/RocketDetailsActivity.kt
@@ -1,4 +1,32 @@
 package com.devpass.spaceapp.presentation
 
-class RocketDetailsActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.devpass.spaceapp.databinding.ActivityRocketDetailsBinding
+
+class RocketDetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRocketDetailsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRocketDetailsBinding.inflate(layoutInflater)
+
+        setUpView()
+        setUpListeners()
+
+        setContentView(binding.root)
+    }
+
+    fun setUpView() {
+        binding.imageViewRocketActivity
+        binding.textViewRocketActivityTitle.text = ""
+        binding.textViewRocketActivityInfo.text = ""
+    }
+
+    fun setUpListeners() {
+        binding.toolbarRocketActivity.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
 }
