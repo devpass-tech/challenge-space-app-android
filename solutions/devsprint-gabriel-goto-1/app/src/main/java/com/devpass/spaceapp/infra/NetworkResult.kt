05cff3b417d90add6caaccb95e3ece6c8d22bfc1
Gabriel Goto <gabrielcosta08@gmail.com>
2022-08-15 23:43:42 +0100

    Add Network Result class

---
+++ solutions/devsprint-gabriel-goto-1/app/src/main/java/com/devpass/spaceapp/infra/NetworkResult.kt
@@ -0,0 +1,23 @@
+package com.devpass.spaceapp.infra
+
+sealed class NetworkResult<T>(
+    val data: T? = null,
+    val message: String? = null
+) {
+
+    class Success<SUCCESS>(data: SUCCESS) : NetworkResult<SUCCESS>(data)
+
+    class Error<ERROR>(message: String?) : NetworkResult<ERROR>(null, message)
+
+    class Loading : NetworkResult<Nothing>()
+}
+
+suspend fun <T> isSuccess(block: suspend () -> T): NetworkResult<T> {
+    return try {
+        val result = block()
+
+        NetworkResult.Success(result)
+    } catch (ex: Exception) {
+        NetworkResult.Error(ex.localizedMessage)
+    }
+}
