db91a7af789f22a81663a155f125b8573672e66d
Lucas Emboava <emboavalucas@hotmail.com>
2022-08-17 21:59:15 -0300

    Alteração na forma de passar dados entre o fragment e a activity

--- solutions/devsprint-gabriel-goto-1/app/src/main/java/com/devpass/spaceapp/presentation/view/RocketFragment.kt
+++ solutions/devsprint-gabriel-goto-1/app/src/main/java/com/devpass/spaceapp/presentation/view/RocketFragment.kt
@@ -1,18 +1,30 @@
 package com.devpass.spaceapp.presentation.view
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.Parcelable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.devpass.spaceapp.databinding.FragmentRocketBinding
 import com.devpass.spaceapp.presentation.RocketDetailsActivity
+import java.lang.reflect.Array.newInstance
 
 class RocketFragment : Fragment() {
 
     private var _binding: FragmentRocketBinding? = null
     private val binding get() = _binding!!
+
+    companion object {
+        fun newInstance(context: Context, args: RocketDetailArgs): Intent {
+            return Intent(context, RocketDetailsActivity::class.java).also {
+
+                it.putExtra("EXTRA_ROCKET_DETAIL_ARGS", args)
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,13 +39,18 @@ class RocketFragment : Fragment() {
             val title = binding.textViewRocketFragmentTitle.text
             val info = binding.textViewRocketFragmentInfo.text
 
-            Intent(requireActivity(), RocketDetailsActivity::class.java).also {
+            //Intent(requireActivity(), RocketDetailsActivity::class.java).also {
 
-                it.putExtra("EXTRA_TITLE", title)
-                it.putExtra("EXTRA_INFO", info)
-                startActivity(it)
+            //it.putExtra("EXTRA_TITLE", title)
+            //it.putExtra("EXTRA_INFO", info)
+            //startActivity(it)
 
-            }
+            //}
+
+
+            val intent = RocketDetailsActivity.newInstance(requireContext(), args)
+            startActivity(intent)
+
         }
 
         return binding.root
@@ -45,18 +62,18 @@ class RocketFragment : Fragment() {
         binding.textViewRocketFragmentInfo.text = ""
     }
 
-    private fun navigateToRocketDetailsActivity(){
-
-
-        }
-
-
-
-
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
     }
-
-
 }
+
+@Parcelize
+data class RocketDetailArgs(
+    val title: String,
+    val info: String,
+    val image: String,
+) : Parcelable
+
+
+
