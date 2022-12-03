893fc0731dbf23a97d19b622c90e237d443805b5
hsilva <higoorcamara@gmail.com>
2022-12-03 12:34:01 +0000

    Base project structure

---
+++ solutions/devsprint-higor-silva-1/app/src/main/java/com/devpass/spaceapp/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.devpass.spaceapp.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.devpass.spaceapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
