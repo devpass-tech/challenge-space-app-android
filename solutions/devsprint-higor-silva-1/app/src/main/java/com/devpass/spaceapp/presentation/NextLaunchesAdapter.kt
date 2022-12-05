f5b87a83551dfe925837406350d874fd1ca2b864
Barbara Romero <bqromero@gmail.com>
2022-12-05 20:39:55 -0300

    create adapter

---
+++ solutions/devsprint-higor-silva-1/app/src/main/java/com/devpass/spaceapp/presentation/NextLaunchesAdapter.kt
@@ -0,0 +1,39 @@
+package com.devpass.spaceapp.presentation
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.devpass.spaceapp.R
+import com.devpass.spaceapp.databinding.ItemLaunchListBinding
+import com.devpass.spaceapp.models.NextLaunchesModel
+
+class NextLaunchesAdapter(private val list: List<NextLaunchesModel>) :
+    RecyclerView.Adapter<NextLaunchesAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val binding = ItemLaunchListBinding.bind(itemView)
+
+        fun bind(nextLaunchesModel: NextLaunchesModel) {
+            binding.ivLaunchList.setImageResource(nextLaunchesModel.image)
+            binding.tvTitleLaunchList.text = nextLaunchesModel.title
+            binding.tvSubtitleLaunchList.text = nextLaunchesModel.subtitle
+            binding.tvStatusLaunchList.text = nextLaunchesModel.status
+            binding.tvPositionLaunchList.text = nextLaunchesModel.position
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+        ViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_launch_list,
+                parent,
+                false
+            )
+        )
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) =
+        holder.bind(list[position])
+
+    override fun getItemCount(): Int = list.size
+}
--- solutions/devsprint-higor-silva-1/app/src/main/java/com/devpass/spaceapp/presentation/list/LaunchListFragment.kt
+++ solutions/devsprint-higor-silva-1/app/src/main/java/com/devpass/spaceapp/presentation/list/LaunchListFragment.kt
@@ -4,21 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import com.devpass.spaceapp.R
 import com.devpass.spaceapp.databinding.FragmentLaunchListBinding
 
 class LaunchListFragment : Fragment() {
+
+    private lateinit var binding: FragmentLaunchListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding: FragmentLaunchListBinding = DataBindingUtil.inflate(
+        binding = FragmentLaunchListBinding.inflate(
             inflater,
-            R.layout.fragment_launch_list,
             container,
             false
         )
