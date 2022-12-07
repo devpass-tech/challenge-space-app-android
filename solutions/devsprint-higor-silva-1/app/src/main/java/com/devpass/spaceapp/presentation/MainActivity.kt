704a664d3a9e22c1f77bfd12c603db271a844231
Barbara Romero <bqromero@gmail.com>
2022-12-07 08:37:12 -0300

    create new toolbar and add rocket emoji

--- solutions/devsprint-higor-silva-1/app/src/main/java/com/devpass/spaceapp/presentation/MainActivity.kt
+++ solutions/devsprint-higor-silva-1/app/src/main/java/com/devpass/spaceapp/presentation/MainActivity.kt
@@ -2,11 +2,18 @@ package com.devpass.spaceapp.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.devpass.spaceapp.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val rocket = "\uD83D\uDE80"
+        toolbar.title = "Space App $rocket"
     }
 }
--- solutions/devsprint-higor-silva-1/app/src/main/java/com/devpass/spaceapp/presentation/list/LaunchListFragment.kt
+++ solutions/devsprint-higor-silva-1/app/src/main/java/com/devpass/spaceapp/presentation/list/LaunchListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.devpass.spaceapp.R
