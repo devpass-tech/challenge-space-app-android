9c1421e3e10c3a49ed6f713ade0e9bf69adc469b
Helvio Siqueira <67134795+HelvioSiqueira@users.noreply.github.com>
2022-12-07 10:14:37 -0300

    feat: Creating DetailsFragment

---
+++ solutions/devsprint-higor-silva-1/app/src/main/java/com/devpass/spaceapp/presentation/launch/DetailsFragment.kt
@@ -0,0 +1,37 @@
+package com.devpass.spaceapp.presentation.launch
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.devpass.spaceapp.databinding.FragmentDetailsBinding
+
+class DetailsFragment : Fragment() {
+
+    private lateinit var txtDetails: TextView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentDetailsBinding.inflate(inflater, container, false)
+
+        txtDetails = binding.txtDetails
+        txtDetails.text = arguments?.getString(EXTRA_DETAILS)
+
+        return binding.root
+    }
+
+    companion object {
+        private const val EXTRA_DETAILS = "details"
+
+        fun newInstance(detais: String) = DetailsFragment().apply {
+            arguments = Bundle().apply {
+                putString(EXTRA_DETAILS, detais)
+            }
+        }
+    }
+}
