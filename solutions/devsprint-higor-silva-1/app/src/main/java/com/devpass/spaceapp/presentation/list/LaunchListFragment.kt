b0934578772c9992ada7392ec22e5ef4b9491dc1
Helvio Siqueira <67134795+HelvioSiqueira@users.noreply.github.com>
2022-12-08 09:50:11 -0300

    feat: Add fun onClickListItem in LaunchListFragment

--- solutions/devsprint-higor-silva-1/app/src/main/java/com/devpass/spaceapp/presentation/list/LaunchListFragment.kt
+++ solutions/devsprint-higor-silva-1/app/src/main/java/com/devpass/spaceapp/presentation/list/LaunchListFragment.kt
@@ -6,11 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction.TRANSIT_FRAGMENT_OPEN
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.devpass.spaceapp.R
 import com.devpass.spaceapp.databinding.FragmentLaunchListBinding
 import com.devpass.spaceapp.models.NextLaunchesModel
 import com.devpass.spaceapp.presentation.adapter.NextLaunchesAdapter
+import com.devpass.spaceapp.presentation.launch.LaunchFragment
 
 class LaunchListFragment : Fragment() {
 
@@ -48,5 +50,15 @@ class LaunchListFragment : Fragment() {
             }
         )
         binding.rvLaunchList.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    fun onClickListItem(nextLaunchClicked: NextLaunchesModel){
+        val fragment = LaunchFragment.newInstance(nextLaunchClicked)
+
+        requireActivity().supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.nav_host_fragment, fragment, LaunchFragment.TAG_LAUNCH)
+            .setTransition(TRANSIT_FRAGMENT_OPEN)
+            .commit()
     }
 }
