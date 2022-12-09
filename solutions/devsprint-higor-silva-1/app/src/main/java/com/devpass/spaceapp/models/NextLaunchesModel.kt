7801050634e5a0299d872af5c38549934ff57672
Helvio Siqueira <67134795+HelvioSiqueira@users.noreply.github.com>
2022-12-09 09:43:42 -0300

    feat: Creating action and argument in launchListFragment

--- solutions/devsprint-higor-silva-1/app/src/main/java/com/devpass/spaceapp/models/NextLaunchesModel.kt
+++ solutions/devsprint-higor-silva-1/app/src/main/java/com/devpass/spaceapp/models/NextLaunchesModel.kt
@@ -1,16 +1,12 @@
 package com.devpass.spaceapp.models
 
-import org.parceler.Parcel
-import org.parceler.ParcelConstructor
+import java.io.Serializable
 
-@Parcel
 data class NextLaunchesModel(
     val image: Int,
     val title: String,
     val subtitle: String,
     val status: String,
     val position: String
-){
-    @ParcelConstructor constructor(): this(0, "", "", "", "")
-}
+): Serializable
 
