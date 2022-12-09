1517ff63377dc036699235fc5ba4a93717c97043
Helvio Siqueira <67134795+HelvioSiqueira@users.noreply.github.com>
2022-12-09 10:24:11 -0300

    feat: Add anim trasition

--- solutions/devsprint-higor-silva-1/app/src/main/java/com/devpass/spaceapp/presentation/list/LaunchListFragment.kt
+++ solutions/devsprint-higor-silva-1/app/src/main/java/com/devpass/spaceapp/presentation/list/LaunchListFragment.kt
@@ -4,17 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction.TRANSIT_FRAGMENT_OPEN
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.devpass.spaceapp.R
 import com.devpass.spaceapp.databinding.FragmentLaunchListBinding
 import com.devpass.spaceapp.models.NextLaunchesModel
 import com.devpass.spaceapp.presentation.adapter.NextLaunchesAdapter
-import com.devpass.spaceapp.presentation.launch.LaunchFragment
-import com.devpass.spaceapp.presentation.launch.LaunchFragmentDirections
 
 class LaunchListFragment : Fragment() {
 
