9cf75f4a447aa082be271fb7b69fc2ac50aef5d5
Helvio Siqueira <67134795+HelvioSiqueira@users.noreply.github.com>
2022-12-10 19:25:01 -0300

    chore: creating getInstance of api

--- solutions/devsprint-higor-silva-1/app/src/main/java/com/devpass/spaceapp/repository/SpacexApi.kt
+++ solutions/devsprint-higor-silva-1/app/src/main/java/com/devpass/spaceapp/repository/SpacexApi.kt
@@ -1,16 +1,30 @@
 package com.devpass.spaceapp.repository
 
 import com.devpass.spaceapp.models.Launch
-import com.devpass.spaceapp.models.Rocket
 import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
-import retrofit2.http.Path
 
 interface SpacexApi {
 
     @GET("launches?order=desc&limit=20")
     suspend fun getLaunchs(): Response<List<Launch>>
 
-    @GET("rockets/{rocketId}")
-    suspend fun getRocket(@Path("rocketId") rocketId: String): Response<Rocket>
+    companion object {
+        var retrofitService: SpacexApi? = null
+
+        fun getInstance(): SpacexApi {
+            if (retrofitService == null) {
+                val retrofit = Retrofit.Builder()
+                    .baseUrl("https://api.spacexdata.com/v3/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+
+                retrofitService = retrofit.create(SpacexApi::class.java)
+            }
+
+            return retrofitService!!
+        }
+    }
 }
