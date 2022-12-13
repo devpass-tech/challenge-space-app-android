194c1395e93f02c9942e5a0836fc9215d527a164
Helvio Siqueira <67134795+HelvioSiqueira@users.noreply.github.com>
2022-12-13 19:37:26 -0300

    chore: add call in api to Launchpad

--- solutions/devsprint-higor-silva-1/app/src/main/java/com/devpass/spaceapp/presentation/launch/LaunchFragment.kt
+++ solutions/devsprint-higor-silva-1/app/src/main/java/com/devpass/spaceapp/presentation/launch/LaunchFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import androidx.viewpager2.widget.ViewPager2
 import coil.load
