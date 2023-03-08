f5553016963d47c3f78cd0b8ab6b455245557863
adenilsonricardo <85114978+adenilsonricardo@users.noreply.github.com>
2023-03-08 16:29:35 -0300

    implementado Retrofit

---
+++ solutions/devsprint-bruno-almeida-2/app/src/main/java/com/devpass/spaceapp/RetrofitService.kt
@@ -0,0 +1,17 @@
+package com.devpass.spaceapp
+
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+const val BASE_URL = "https://api.spacexdata.com/v5/"
+
+object RetrofitService {
+
+    val gson = GsonBuilder().setLenient().create()
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+}
