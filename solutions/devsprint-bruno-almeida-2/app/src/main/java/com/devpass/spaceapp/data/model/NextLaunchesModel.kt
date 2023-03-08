abbef29b97af1205b9f44fc6816577d104f7f81a
adenilsonricardo <85114978+adenilsonricardo@users.noreply.github.com>
2023-03-07 22:31:14 -0300

    implementado LaunchCellItem

--- solutions/devsprint-bruno-almeida-2/app/src/main/java/com/devpass/spaceapp/data/model/NextLaunchesModel.kt
+++ solutions/devsprint-bruno-almeida-2/app/src/main/java/com/devpass/spaceapp/data/model/NextLaunchesModel.kt
@@ -1,5 +1,9 @@
 package com.devpass.spaceapp.data.model
 
-class NextLaunchesModel {
-
-}
+data class NextLaunchesModel(
+    val number : Int,
+    val name : String,
+    val date : Int,
+    val status : Boolean,
+    val image: String
+)
