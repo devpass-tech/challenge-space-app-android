4ab1d2266dfc87c4f40616ebad991a2934bf10ab
gisele <gisesonia@gmail.com>
2023-03-09 15:27:10 -0300

    Adapter configurado na recyclerView

--- solutions/devsprint-bruno-almeida-2/app/src/main/java/com/devpass/spaceapp/presentation/NextLaunchesActivity.kt
+++ solutions/devsprint-bruno-almeida-2/app/src/main/java/com/devpass/spaceapp/presentation/NextLaunchesActivity.kt
@@ -1,19 +1,60 @@
 package com.devpass.spaceapp.presentation
 
-
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.devpass.spaceapp.data.model.NextLaunchesModel
+import com.devpass.spaceapp.databinding.ActivityMainBinding
 
-import androidx.recyclerview.widget.RecyclerView
-import com.devpass.spaceapp.R
 
 class NextLaunchesActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: NextLaunchesAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val recyclerView: RecyclerView = findViewById(R.id.nextlaunchrecyclerview)
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        // Definir o layout manager da RecyclerView
+        binding.nextlaunchrecyclerview.layoutManager = LinearLayoutManager(this)
+
+        adapter = NextLaunchesAdapter(getData(), object : NextLaunchesListener {
+            override fun onItemClick(position: Int, launch: NextLaunchesModel) {
+                val launchBundle = Bundle().apply {
+                    putInt("launchCost", launch.number)
+                    putString("launchImageUrl", launch.image)
+                    putString("launchName", launch.name)
+                }
+                val launchIntent = Intent(this@NextLaunchesActivity, LaunchDetailsActivity::class.java).apply {
+                    putExtras(launchBundle)
+                }
+                startActivity(launchIntent)
+            }
+        })
+
+        // Atribuir o adapter à RecyclerView
+        binding.nextlaunchrecyclerview.adapter = adapter
+
+    }
+
+    // Função temporária para gerar os dados
+    private fun getData(): List<NextLaunchesModel> {
+        val data = mutableListOf<NextLaunchesModel>()
+        for (i in 1..10) {
+            data.add(
+                NextLaunchesModel(
+                    i,
+                    "Rocket $i",
+                    i * 1000000,
+                    i % 2 == 0,
+                    "https://via.placeholder.com/150?text=Rocket+$i"
+                )
+            )
+        }
+        return data
     }
 }
+
