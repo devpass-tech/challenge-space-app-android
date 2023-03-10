456c7cd98eba968c0b7952a2dc41e4fe43132066
adenilsonricardo <85114978+adenilsonricardo@users.noreply.github.com>
2023-03-10 17:05:45 -0300

    implementado no model as data class com o que sera necessario para o app.

--- solutions/devsprint-bruno-almeida-2/app/src/main/java/com/devpass/spaceapp/presentation/LaunchCellItem.kt
+++ solutions/devsprint-bruno-almeida-2/app/src/main/java/com/devpass/spaceapp/presentation/LaunchCellItem.kt
@@ -2,6 +2,7 @@ package com.devpass.spaceapp.presentation
 
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.devpass.spaceapp.data.model.NextLaunchesModel
 import com.devpass.spaceapp.databinding.ActivityLaunchCellItemBinding
 
 class LaunchCellItem(
