a9fd401265c61348555c46b89ffbd161d4a13322
gisele <gisesonia@gmail.com>
2023-03-09 22:41:33 -0300

    Adicionando Serializable

--- solutions/devsprint-bruno-almeida-2/app/src/main/java/com/devpass/spaceapp/data/model/NextLaunchesModel.kt
+++ solutions/devsprint-bruno-almeida-2/app/src/main/java/com/devpass/spaceapp/data/model/NextLaunchesModel.kt
@@ -1,9 +1,11 @@
 package com.devpass.spaceapp.data.model
 
+import java.io.Serializable
+
 data class NextLaunchesModel(
     val number : Int,
     val name : String,
     val date : Int,
     val status : Boolean,
     val image: String
-)
+): Serializable
